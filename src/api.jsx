5fcb1a1a60a7cef93bd2ab60fc5ae14ae2d36459
Janilson Coimbra <janilson.coimbra@gmail.com>
2022-03-18 13:21:04 -0300

    Implements methods post e put

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,4 @@
 import axios from "axios";
 const apiUrl = [{baseURL:"https://api-agencia-na-bahia-turismo.herokuapp.com"}, {baseURL:"http://localhost:8080"}]
-const api = axios.create(apiUrl[1]);
+const api = axios.create(apiUrl[0]);
 export default api
